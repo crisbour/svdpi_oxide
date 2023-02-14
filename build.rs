44df5e09154272ade3c10c0340e2f80e008f53c2
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2023-02-14 11:20:10 +0000

    Recompile if change

--- build.rs
+++ build.rs
@@ -3,6 +3,8 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+use bindgen::CargoCallbacks;
+
 fn main() {
     //println!("cargo:rustc-link-lib=svdpi");
     println!("cargo:rerun-if-changed=wrapper.h");
@@ -10,6 +12,7 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .parse_callbacks(Box::new(CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
